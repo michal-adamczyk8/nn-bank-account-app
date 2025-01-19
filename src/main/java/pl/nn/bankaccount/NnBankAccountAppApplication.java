14add1b54161068937d4bf60fb537edba6ced8d5
michal-adamczyk8 <adamczyk888@gmail.com>
2025-01-19 20:12:34 +0100

    Project setup

---
+++ src/main/java/pl/nn/bankaccount/NnBankAccountAppApplication.java
@@ -0,0 +1,13 @@
+package pl.nn.bankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnBankAccountAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnBankAccountAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/nn/bankaccount/NnBankAccountAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nn.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnBankAccountAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
